c8413bf76d4ce88b72915858399f651073b2878e
Engineering User <engineeringuser@10-248-101-252.wireless.oregonstate.edu>
2018-09-14 09:48:45 -0700

    feat(styles): add tables styles for table proof of concept

--- src/styles.js
+++ src/styles.js
@@ -6,13 +6,34 @@ import { StyleSheet } from '@react-pdf/renderer';
 export default StyleSheet.create({
   page: {
     textAlign: 'center',
+    padding: 30
   },
   column: {
     flexGrow: 1,
   },
   text: {
+    padding: 15,
     margin: 10,
     fontSize: 10,
     color: 'black',
   },
+  tr: {
+    fontSize: 15,
+    padding: 10
+  },
+  tdl: {
+    textAlign: "left",
+    width: 125,
+    border: 1,
+    borderColor: 'black',
+  },
+
+  tdr: {
+    padding: 20,
+    textAlign: 'right',
+    width: 375,
+    border: 1,
+    borderColor: 'black'
+  }
+
 });
