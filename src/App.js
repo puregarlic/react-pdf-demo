56adcb150dcf168a4fc40a80526b15337e6eb189
Engineering User <engineeringuser@10-248-101-252.wireless.oregonstate.edu>
2018-09-14 09:48:09 -0700

    feat(table): add mark down for table proof of concept

--- src/App.js
+++ src/App.js
@@ -15,11 +15,21 @@ const App = () => (
       <h2>React PDF DOM bindings</h2>
     </div>
     <Document title="Testing" author="CASS" subject="Rendering with react-pdf">
-      <Page wrap style={style.page}>
+      <Page wrap style={style.page} size="A4" wrap>
         <View>
-          <View>
-            <Text style={style.text}>Hello, world</Text>
-          </View>
+          <View style={style.tr}>
+              <Text style={style.tdl}>Stuff and things</Text>
+              <Text style={style.tdr}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta neque suscipit lorem aliquam, et ultricies eros tincidunt. Maecenas turpis ipsum, dignissim at facilisis id, lobortis eu enim. Nulla id risus dictum, pulvinar lorem quis, posuere nisi. Nulla sit amet cursus est. Sed vel tristique justo, a lobortis massa. Donec ut suscipit sem. Proin dolor sapien, eleifend sed massa ac, venenatis lacinia purus. Curabitur nec lorem accumsan, tristique risus non, consequat dolor. Vivamus ultrices tempor nunc. Aliquam bibendum, felis suscipit placerat porta, erat dolor aliquam lectus, aliquam accumsan diam mauris sed lorem. Fusce vestibulum arcu eu mi consectetur luctus. Aliquam erat volutpat.
+
+Mauris at neque ac leo aliquet viverra vitae id mi. Nam lobortis mauris vitae eros efficitur suscipit sit amet eget enim. Nulla quis augue eget massa varius ultrices ut id risus. Mauris scelerisque nulla sed justo gravida tempor. Sed sit amet gravida urna. Quisque id ligula metus. Aliquam ut consequat augue. Nulla feugiat ipsum non massa dignissim, at posuere turpis pharetra. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In dolor odio, pretium sed mollis at, commodo ut orci. Phasellus eget efficitur mi, sit amet hendrerit diam. Pellentesque sodales lorem ac posuere egestas. Praesent semper ornare lectus.</Text>
+            </View>
+
+            <View style={style.tr}>
+              <Text style={style.tdl}>Stuff and things</Text>
+              <Text style={style.tdr}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta neque suscipit lorem aliquam, et ultricies eros tincidunt. Maecenas turpis ipsum, dignissim at facilisis id, lobortis eu enim. Nulla id risus dictum, pulvinar lorem quis, posuere nisi. Nulla sit amet cursus est. Sed vel tristique justo, a lobortis massa. Donec ut suscipit sem. Proin dolor sapien, eleifend sed massa ac, venenatis lacinia purus. Curabitur nec lorem accumsan, tristique risus non, consequat dolor. Vivamus ultrices tempor nunc. Aliquam bibendum, felis suscipit placerat porta, erat dolor aliquam lectus, aliquam accumsan diam mauris sed lorem. Fusce vestibulum arcu eu mi consectetur luctus. Aliquam erat volutpat.
+
+Mauris at neque ac leo aliquet viverra vitae id mi. Nam lobortis mauris vitae eros efficitur suscipit sit amet eget enim. Nulla quis augue eget massa varius ultrices ut id risus. Mauris scelerisque nulla sed justo gravida tempor. Sed sit amet gravida urna. Quisque id ligula metus. Aliquam ut consequat augue. Nulla feugiat ipsum non massa dignissim, at posuere turpis pharetra. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In dolor odio, pretium sed mollis at, commodo ut orci. Phasellus eget efficitur mi, sit amet hendrerit diam. Pellentesque sodales lorem ac posuere egestas. Praesent semper ornare lectus.</Text>
+            </View>
         </View>
       </Page>
     </Document>
